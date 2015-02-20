794e667c19a90d1640ede3727122ee6c1fb9b81a
unknown <sharma.susheel4@gmail.com>
2015-02-20 20:02:18 +0530

    commit all

---
+++ Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Drawing;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+    SqlCommand cmd = new SqlCommand();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        cmd = new SqlCommand("insert into usertbl(name,address)values('"+TextBox1.Text+"','"+TextBox2.Text+"')",sqlcon);
+        sqlcon.Open();
+
+        cmd.ExecuteNonQuery();
+
+        sqlcon.Close();
+
+        Label lbl = new Label();
+
+        lbl.Text = "data saved...";
+
+        form1.Controls.Add(lbl);
+
+        lbl.ForeColor = Color.Red;
+
+
+
+    }
+}
